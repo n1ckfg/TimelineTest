62c854dbdffb46564de2c8b34a12ccef5b43a325
Nick Fox-Gieg <nick@fox-gieg.com>
2017-09-15 14:48:30 -0400

    cleanup

---
+++ Assets/Scripts/VideoTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoTime : MonoBehaviour {
+
+    public VideoPlayer video;
+    public float time = 0f;
+
+    private void Awake() {
+        if (video == null) video = GetComponent<VideoPlayer>();
+    }
+
+    private void Start() {
+        //video.playbackSpeed = 0f;
+    }
+
+    private void Update() {
+        video.time = time;		
+	}
+
+}
